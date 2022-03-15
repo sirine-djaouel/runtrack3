8fde7f376b44743a42b744f4e48f6d02ed4b5974
Sirine <sirine.djaouel@laplateforme.io>
2022-03-15 11:35:14 +0100

    premier comit

---
+++ job05/script.js
@@ -0,0 +1,17 @@
+
+
+//console.log(JoursSemaines);
+
+
+function AfficherJoursSemaines () {
+
+    
+    var JoursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    for(let i=0; i<JoursSemaines.lenght; i++){
+
+        console.log(JoursSemaines[i]);
+    }
+
+}
+AfficherJoursSemaines();
